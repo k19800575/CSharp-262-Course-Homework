6e8bb6909ef260a826b7d1881a29c936c54e59a2
Jun-Jie Yang <k19800575@gmail.com>
2015-12-23 15:12:39 +0800

    初學者入門作業
    在Array中 實作基本資料處理:
    1. 實作Sum() method: 將內部的values加起來得到總和
    2. 實作Cusum() method(類似matlab cusum): 將數字累加起來
    例如:原本是 {1,2,4,8} ---> { 1,3,7,15}
    3. 實作Add(double value): 對每個元素加一個數字
    4. 實作Sub(double value): 對每個元素減一個數字
    5. 實作Multiply(double value): 對每個元素程一個數字
    6. 實作Div(double value): 對每個元素除一個數字
    7. 上述方法能夠chaining意即:
    可以實現類似這樣的操作
    numbers.Add(1).Sub(4).Multiply(10).Sum()

---
+++ [CS262]Beginner HomeWork/Beginner/Array.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginner
+{
+    internal class Array
+    {
+        public double[] variables;
+
+        public double total = 0;
+
+        //建構式(constructor)用來初始化
+        public Array(int length)
+        {
+            variables = new double[length];
+
+            for (int count = 0; count < length; count++)
+            {
+                variables[count] = 0; //初始化為0
+            }
+        }
+
+        public Array Add(double value)
+        {
+            double[] addArray = new double[this.variables.Length];
+
+            for (int count = 0; count < variables.Length; count++)
+            {
+                addArray[count] = this.variables[count] + value;
+            }
+
+            this.variables = addArray;
+
+            return this;
+        }
+
+        public Array Sub(double value)
+        {
+            double[] subArray = new double[this.variables.Length];
+
+            for (int count = 0; count < variables.Length; count++)
+            {
+                subArray[count] = this.variables[count] - value;
+            }
+
+            this.variables = subArray;
+
+            return this;
+        }
+
+        public Array Multiply(double value)
+        {
+            double[] multiplyArray = new double[this.variables.Length];
+
+            for (int count = 0; count < variables.Length; count++)
+            {
+                multiplyArray[count] = this.variables[count] * value;
+            }
+
+            this.variables = multiplyArray;
+
+            return this;
+        }
+
+        public Array Div(double value)
+        {
+            double[] divAarray = new double[this.variables.Length];
+
+            for (int count = 0; count < variables.Length; count++)
+            {
+                divAarray[count] = this.variables[count] / value;
+            }
+
+            this.variables = divAarray;
+
+            return this;
+        }
+
+        public Array Sum()
+        {
+            double sum = 0;
+
+            for (int count = 0; count < variables.Length; count++)
+            {
+                sum += this.variables[count];
+            }
+
+            this.total = sum;
+
+            return this;
+        }
+
+        public Array Cusum()
+        {
+            double[] cusumAarray = new double[this.variables.Length];
+
+            cusumAarray = this.variables;
+
+            for (int i = variables.Length - 1; i >= 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    cusumAarray[i] += cusumAarray[i - j];
+                }
+            }
+
+            this.variables = cusumAarray;
+
+            return this;
+        }
+
+        public void Print()
+        {
+            for (int count = 0; count < this.variables.Length; count++)
+            {
+                Console.WriteLine("Array[{0}] = {1}", count, variables[count]);
+            }
+
+            Console.Write("\n");
+            Console.WriteLine("Sun:{0}\n", this.total);
+        }
+    }
+}
---
+++ [CS262]Beginner HomeWork/Beginner/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginner
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double AddNumber = 0;
+            double SubNumber = 0;
+            double MultiplyNumber = 0;
+            double DivNumber = 0;
+
+            Array array = new Array(10);
+            array.variables[0] = 70;
+            array.variables[1] = 65;
+            array.variables[2] = 85;
+            array.variables[3] = 95;
+            array.variables[4] = 83;
+            array.variables[5] = 95;
+            array.variables[6] = 93;
+            array.variables[7] = 84;
+            array.variables[8] = 95;
+            array.variables[9] = 93;
+
+            Console.WriteLine("---Array Numbers---");
+            array.Print();
+
+            Console.WriteLine("---Cusum()---");
+            array.Cusum().Print();
+
+            Console.WriteLine("---Input---");
+            Console.Write("Add = ");
+            AddNumber = double.Parse(Console.ReadLine());
+            Console.Write("Sub = ");
+            SubNumber = double.Parse(Console.ReadLine());
+            Console.Write("Multiply = ");
+            MultiplyNumber = double.Parse(Console.ReadLine());
+            Console.Write("Div = ");
+            DivNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("\n---Output---\n");
+            array.Add(AddNumber).Sub(SubNumber).Multiply(MultiplyNumber).Div(DivNumber).Sum().Print();
+        }
+    }
+}
